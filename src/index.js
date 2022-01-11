39e571762c8ba0ff5ba12e524a493522f89680d4
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-10 20:39:18 -0400

    Added React Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchContextProvider from "./store/search-store";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <SearchContextProvider>
-    <App />
-  </SearchContextProvider>,
+  <BrowserRouter>
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
