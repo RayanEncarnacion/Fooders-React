8d62e57ba087be277d36ac920c8ae45eea06f6c2
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-10 20:26:38 -0400

    Routing done and display welcome/error messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import Navigation from "./components/Navigation";
 import RecipeDetails from "./components/RecipeDetails";
 import Recipes from "./components/Recipes";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { SearchContext } from "./store/search-store";
 
 function App() {
+  const { searchedRecipes, requestError } = useContext(SearchContext);
+
   return (
     <>
-      <Navigation />
-      <RecipeDetails />
-      <Recipes />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navigation />
+              {}
+              {requestError && <h3 className="no-recipes">{requestError}</h3>}
+              {searchedRecipes.length === 0 && (
+                <h3 className="no-recipes">
+                  Search for your favorite dish or ingredient!
+                </h3>
+              )}
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="/:food" element={<Recipes />}>
+            <Route path=":id" element={<RecipeDetails />} />
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 }
