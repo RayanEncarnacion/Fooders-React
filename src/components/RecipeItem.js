03f429c56b961b1e9d165fd6b24f5f3210718720
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-12 12:32:30 -0400

    Added variants props to pass transition variants

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -3,9 +3,13 @@ import { motion } from "framer-motion";
 
 const hover = { scale: 1.03 };
 
-const RecipeItem = ({ title, img, publisher, onClick }) => {
+const RecipeItem = ({ title, img, publisher, onClick, variants }) => {
   return (
-    <motion.div onClick={onClick} whileHover={hover} className={classes.recipe}>
+    <motion.div
+      onClick={onClick}
+      variants={variants}
+      className={classes.recipe}
+    >
       <img src={img} alt={`${title} image`} />
       <div>
         <h4 className="recipe-title">{title}</h4>
